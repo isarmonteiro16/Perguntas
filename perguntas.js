813a5c5bc4dc750e6757b2efeb1785cf602a5726
isarmonteiro16 <00001114446373sp@al.educacao.sp.gov.br>
2024-11-22 15:40:29 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,65 @@
+criaCartao(
+    'Geografia', 
+    'Qual a capital do Brasil?',
+    'Brasília'
+)
+
+criaCartao(
+    'Geografia', 
+    'Em qual país ocorreu as Olímpiadas de 2024?',
+    'França'
+)
+
+criaCartao(
+    'Geografia', 
+    'Qual é o maior deserto do mundo?',
+    'Deserto do Saara'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a língua oficial da China?',
+    'Mandarim'
+)
+
+criaCartao(
+    'Geografia',
+    'Em que ano o Brasil foi descoberto?',
+    '1500'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital dos Estados Unidos?',
+    'Washington'
+)
+criaCartao(
+    'Geografia',
+    'Qual o significado da sigla ONU?',
+    'Organização das Nações Unidas'
+)
+criaCartao(
+    'Biologia',
+    'O que é gene?',
+    'Segmento de características herdadas geneticamente'
+)
+criaCartao(
+    'Biologia',
+    'O que é a genética?',
+    'Área da biologia que estuda a herediteridade'
+)
+criaCartao(
+    'História',
+    'Quem descobriu o Brasil?',
+    'Pedro Álvares Cabral'
+)
+criaCartao(
+    'História',
+    'Quando começou a revolução francesa?',
+    '1789'
+)
+criaCartao(
+    'História',
+    'O que foi a lei Áurea?',
+    'Fim da escravidão'
+)
